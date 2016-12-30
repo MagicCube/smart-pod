73377128c834e1d70f94061ee4ef5a5659198f76
Henry Li <henry1943@163.com>
2016-12-30 11:09:40 +0800

    Add WiFi network auto detection

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,13 @@
 #include <VS1053.h>
 #include <log.h>
 
-#include "./buffer/MediaInputBuffer.h"
-#include "./buffer/MediaOutputBuffer.h"
+#include "./stream/MediaInputBuffer.h"
+#include "./stream/MediaOutputBuffer.h"
+#include "./util/URLParser.h"
 
 extern "C" {
 #include "user_interface.h"
 }
-
-// WiFi Settings
-#define WIFI_SSID "Henry's TP-LINK"
-//#define WIFI_SSID "Henry's iPhone 6"
-#define WIFI_PWD "13913954971"
 
 // VS1053 Settings
 #define VS1053_CS_PIN D1
@@ -49,7 +45,7 @@ File fileInputStream; // Input stream for local files.
 
 
 // Function Declarations
-void setupWiFi();
+bool setupWiFi();
 bool playLocalFile(String path);
 bool playRemoteUrl(String url);
 
@@ -58,11 +54,14 @@ void setup()
 {
     Serial.begin(115200);
 
+    Serial.println("\n\n");
+    log("smart-radio is now starting...");
+
     // Set to 160 MHz in order to get better I/O performance
     // With ESP8266 running at 80 MHz, it is capable of handling up to 256 kb bitrate.
     // With ESP8266 running at 160 MHz, it is capable of handling up to 320 kb bitrate.
     log("Setting CPU frequency to 160Mhz...");
-    system_update_cpu_freq(160);
+    system_update_cpu_freq(80);
 
     // Here we use SPIFFS(ESP8266 built-in File System) to store stations and other settings,
     // as well as short sound effects.
@@ -78,10 +77,13 @@ void setup()
     SPI.begin();
     vs1053.begin();
     // Set the initial volume
-    vs1053.setVolume(80);
+    vs1053.setVolume(70);
+
+    log("smart-radio is now running...");
 
     // playLocalFile("/test.mp3");
     playRemoteUrl("http://lhttp.qingting.fm/live/387/64k.mp3");
+    //playRemoteUrl("http://m2.music.126.net/NG4I9FVAm9jCQCvszfLB8Q==/1377688074172063.mp3");
 }
 
 void loop()
@@ -104,17 +106,59 @@ void loop()
 }
 
 
-void setupWiFi()
+bool setupWiFi()
 {
     WiFi.mode(WIFI_STA);
-    WiFi.begin(WIFI_SSID, WIFI_PWD);
-    // log("Connecting to %s", WIFI_STA);
+    log("Scanning WiFi...");
+    int ssidCount = WiFi.scanNetworks();
+    if (ssidCount == -1)
+    {
+        log("Couldn't get a WiFi connection.");
+        return false;
+    }
+
+    String WIFI_SSID = "none";
+    String WIFI_PWD;
+    for (int i = 0; i < ssidCount; i++)
+    {
+        String ssid = WiFi.SSID(i);
+        if (ssid.equals("Henry's Living Room 2.4GHz"))
+        {
+            WIFI_SSID = ssid;
+            WIFI_PWD = "13913954971";
+            break;
+        }
+        else if (ssid.equals("Henry's TP-LINK"))
+        {
+            WIFI_SSID = ssid;
+            WIFI_PWD = "13913954971";
+            break;
+        }
+        else if (ssid.equals("Henry's iPhone 6"))
+        {
+            WIFI_SSID = ssid;
+            WIFI_PWD = "13913954971";
+            // Don't break, cause this will connect to 4G network.
+            // It's absolutely not a first choise.
+        }
+    }
+    if (WIFI_SSID.equals("none"))
+    {
+        log("Couldn't find a recognized WiFi connection.");
+        return false;
+    }
+
+    WiFi.begin(WIFI_SSID.c_str(), WIFI_PWD.c_str());
+    Serial.print("<i> Connecting to ");
+    Serial.print(WIFI_SSID);
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
         Serial.print(".");
     }
-    // log(WiFi.localIP().toString());
+    Serial.println("");
+    log("Got IP: %s", WiFi.localIP().toString().c_str());
+    return true;
 }
 
 bool playLocalFile(String path)
@@ -131,21 +175,24 @@ bool playLocalFile(String path)
     return true;
 }
 
-bool playRemoteUrl(String url)
+bool playRemoteUrl(String urlString)
 {
-    log("Loading %s...", url.c_str());
-    const int port = 80;
-    //const String host = "lhttp.qingting.fm";
-    //const String path = "/live/387/64k.mp3";
-    const String host = "m2.music.126.net";
-    const String path = "/jdhQEW6xQVpWiYmfD7p9Ag==/3088528162423768.mp3";
-    if (httpClient.connect(host.c_str(), port))
+    log("Loading %s...", urlString.c_str());
+    URL url = parseURL(urlString);
+    if (url.isValid)
     {
-        httpClient.print(String("GET ") + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" +
-                         "Connection: close\r\n\r\n");
-        mediaInputStream = &httpClient;
-        log("%s has been loaded.", url.c_str());
-        return true;
+        if (httpClient.connect(url.host.c_str(), url.port))
+        {
+            httpClient.print(String("GET ") + url.path + " HTTP/1.1\r\n" + "Host: " + url.host +
+                             "\r\n" + "Connection: close\r\n\r\n");
+            mediaInputStream = &httpClient;
+            log("%s has been loaded.", urlString.c_str());
+            return true;
+        }
+    }
+    else
+    {
+        log("Invalid URL.");
     }
     return false;
 }
