253ef5a2333b70d4b6fc18582ccd944c56fc22ec
Henry Li <henry1943@163.com>
2016-12-31 17:44:48 +0800

    wording

--- src/player/MediaPlayer.cpp
+++ src/player/MediaPlayer.cpp
@@ -41,7 +41,7 @@ bool MediaPlayer::open(String location)
 
     if (_mediaStream && _mediaStream->isValid())
     {
-        Console::debug("File size / First Chunk size: %d", _mediaStream->totalSize());
+        Console::debug("File size / First chunk size: %d", _mediaStream->totalSize());
         return true;
     }
     else
